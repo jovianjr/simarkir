703d0b73270449b9f38b35e0e530ac17c110afc7
jovian <alphonsusjovian@gmail.com>
2023-06-03 15:37:16 +0700

    feat(summary): integrate summary sidemenu

--- src/Components/SideMenu.js
+++ src/Components/SideMenu.js
@@ -1,56 +1,124 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function SideMenu(props) {
-    return(
-        <section className='w-[100%] bg-white overview-x-hidden p-0 m-0'>
-            <div className="absolute text-white h-screen w-[27%] bg-[url('../assets/images/main-bg-simarkir.webp')] rounded-r-3xl py-14 flex flex-col justify-between">
-                <section className='flex flex-col w-full text-center justify-center px-8 py-0'>
-                    <div className='mb-6 w-full flex flex-col items-center'>
-                        <Image src="/icon-park.png" alt='Simbol Parkir' className='mb-2' height={48} width={48}/>
-                        <h1 className='text-[24px] font-bold leading-3 mb-3'>Simarkir</h1>
-                        <p className='text-[12px] font-thin mb-4'>Sistem Manajemen Parkir DTETI</p>
-                        <p className='text-[16px]'>Senin, 12 Maret 2023<br/>15:00</p>
-                    </div>
-                    <div className="flex flex-col gap-2 text-left w-full">
-                        <Link href='/' className='w-full bg-neutral-50/20 rounded-sm drop-shadow-md px-2 py-0.5 flex items-center gap-2'>
-                            <Image src="/home.svg" alt='Simbol Parkir' className='mb-2' height={30} width={30}/>
-                            <p className='text-[14px]'>Dashboard</p>
-                        </Link>
-                        <Link href='/mplapangan' className='w-full bg-neutral-50/20 rounded-sm drop-shadow-md px-2 py-0.5 flex items-center gap-2'>
-                            <Image src="/car.svg" alt='Simbol Parkir' className='mb-2' height={30} width={30}/>
-                            <p className='text-[14px]'>Manajemen Kendaraan</p>
-                        </Link>
-                        <Link href='/mpakademik' className='w-full bg-neutral-50/20 rounded-sm drop-shadow-md px-2 py-0.5 flex items-center gap-2'>
-                            <Image src="/search.svg" alt='Simbol Parkir' className='mb-2' height={30} width={30}/>
-                            <p className='text-[14px]'>Register</p>
-                        </Link>
-                    </div>
-                </section>
-                <section className='w-full px-8 py-0'>
-                    <p className='mb-3 mt-3'>Kapasitas Kendaraan</p>
-                    <div className="flex flex-col gap-2 text-left w-full text-sm">
-                        <div className='w-full bg-neutral-50/20 rounded-sm drop-shadow-md px-2 py-0.5 flex items-center justify-between'>
-                            <div className='flex items-center gap-2'>
-                                <Image src="/car.svg" alt='Simbol Parkir' className='mb-2' height={30} width={30}/>
-                                <p className='text-[14px]'>Mobil</p>
-                            </div>
-                            <p>12/20</p>
-                        </div>
-                        <div className='w-full bg-[#DE2C13] rounded-sm drop-shadow-md px-2 py-0.5 flex items-center justify-between'>
-                            <div className='flex items-center gap-2'>
-                                <Image src="/motorbike.svg" alt='Simbol Parkir' className='mb-2' height={30} width={30}/>
-                                <p className='text-[14px]'>Sepeda Motor</p>
-                            </div>
-                            <p>22/20</p>
-                        </div>
-                    </div>
-                </section>
-            </div>
-            <div className='absolute w-[73%] top-0 right-0 box-border'>
-                {props.children}
-            </div>
-        </section>
-    )
+	const [motor, setMotor] = useState([]);
+	const [mobil, setMobil] = useState([]);
+
+	useEffect(() => {
+		const fetchLogCount = async () => {
+			try {
+				const response = await fetch('/api/summary');
+				const data = await response.json();
+
+				const formatedData = data.reduce((obj, item) => {
+					const { count, jenis_kendaraan, nama, kapasitas } = item;
+
+					if (!obj[jenis_kendaraan]) {
+						obj[jenis_kendaraan] = {
+							parkir: nama,
+							kapasitas: kapasitas,
+							count: parseInt(count),
+						};
+					} else {
+						obj[jenis_kendaraan].count += parseInt(count);
+					}
+
+					return obj;
+				}, {});
+
+				setMotor(formatedData.motor);
+				setMobil(formatedData.mobil);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchLogCount();
+	}, []);
+
+	return (
+		<section className="overview-x-hidden m-0 w-[100%] bg-white p-0">
+			<div className="absolute flex h-screen w-[27%] flex-col justify-between rounded-r-3xl bg-[url('../assets/images/main-bg-simarkir.webp')] py-14 text-white">
+				<section className="flex w-full flex-col justify-center px-8 py-0 text-center">
+					<div className="mb-6 flex w-full flex-col items-center">
+						<Image
+							src="/icon-park.png"
+							alt="Simbol Parkir"
+							className="mb-2"
+							height={48}
+							width={48}
+						/>
+						<h1 className="mb-3 text-[24px] font-bold leading-3">Simarkir</h1>
+						<p className="mb-4 text-[12px] font-thin">Sistem Manajemen Parkir DTETI</p>
+						<p className="text-[16px]">
+							Senin, 12 Maret 2023
+							<br />
+							15:00
+						</p>
+					</div>
+					<div className="flex w-full flex-col gap-2 text-left">
+						<Link
+							href="/"
+							className="flex w-full items-center gap-2 rounded-sm bg-neutral-50/20 px-2 py-0.5 drop-shadow-md"
+						>
+							<Image src="/home.svg" alt="Simbol Parkir" className="mb-2" height={30} width={30} />
+							<p className="text-[14px]">Dashboard</p>
+						</Link>
+						<Link
+							href="/mplapangan"
+							className="flex w-full items-center gap-2 rounded-sm bg-neutral-50/20 px-2 py-0.5 drop-shadow-md"
+						>
+							<Image src="/car.svg" alt="Simbol Parkir" className="mb-2" height={30} width={30} />
+							<p className="text-[14px]">Manajemen Kendaraan</p>
+						</Link>
+						<Link
+							href="/mpakademik"
+							className="flex w-full items-center gap-2 rounded-sm bg-neutral-50/20 px-2 py-0.5 drop-shadow-md"
+						>
+							<Image
+								src="/search.svg"
+								alt="Simbol Parkir"
+								className="mb-2"
+								height={30}
+								width={30}
+							/>
+							<p className="text-[14px]">Register</p>
+						</Link>
+					</div>
+				</section>
+				<section className="w-full px-8 py-0">
+					<p className="mb-3 mt-3">Kapasitas Kendaraan</p>
+					<div className="flex w-full flex-col gap-2 text-left text-sm">
+						<div className="flex w-full items-center justify-between rounded-sm bg-neutral-50/20 px-2 py-0.5 drop-shadow-md">
+							<div className="flex items-center gap-2">
+								<Image src="/car.svg" alt="Simbol Parkir" className="mb-2" height={30} width={30} />
+								<p className="text-[14px]">Mobil</p>
+							</div>
+							<p>
+								{mobil?.count ?? 0}/{mobil?.kapasitas ?? '10'}
+							</p>
+						</div>
+						<div className="flex w-full items-center justify-between rounded-sm bg-[#DE2C13] px-2 py-0.5 drop-shadow-md">
+							<div className="flex items-center gap-2">
+								<Image
+									src="/motorbike.svg"
+									alt="Simbol Parkir"
+									className="mb-2"
+									height={30}
+									width={30}
+								/>
+								<p className="text-[14px]">Sepeda Motor</p>
+							</div>
+							<p>
+								{motor?.count ?? 0}/{motor?.kapasitas ?? '10'}
+							</p>
+						</div>
+					</div>
+				</section>
+			</div>
+			<div className="absolute right-0 top-0 box-border w-[73%]">{props.children}</div>
+		</section>
+	);
 }
--- src/pages/api/summary.js
+++ src/pages/api/summary.js
@@ -1,8 +1,26 @@
-export default function summary(req, res) {
+import db from '@/utils/db';
+
+export default async function handler(req, res) {
 	if (req.method === 'GET') {
-		res.status(200).json({ message: 'GET request successful' });
+		try {
+			const query = `
+			SELECT COUNT(lk.id), k.jenis_kendaraan, p.nama, p.kapasitas
+			FROM log_kendaraan lk
+			JOIN kendaraan k ON lk.kendaraan_id = k.id
+			JOIN parkir p ON lk.parkiran_id = p.id
+			WHERE 
+			DATE_TRUNC('day', lk.waktu_masuk) = CURRENT_DATE
+			and DATE_TRUNC('day', lk.waktu_keluar) = CURRENT_DATE  
+			GROUP BY k.jenis_kendaraan, p.nama, p.kapasitas
+      `;
+			const result = await db.any(query);
+
+			res.status(200).json(result);
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: 'Internal server error' });
+		}
 	} else {
-		res.setHeader('Allow', ['GET']);
-		res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+		res.status(405).json({ message: 'Method not allowed' });
 	}
 }
