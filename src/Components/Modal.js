9d1e9b5e0d0720cb6e1be1282a5c0d630949863e
chrstnale <christianale85@gmail.com>
2023-06-07 16:35:21 +0700

    Added modal component

---
+++ src/Components/Modal.js
@@ -0,0 +1,31 @@
+/**
+ * @typedef {Object} TextProps
+ * @property {React.ComponentType<SvgIconProps>} [StartIcon]
+ */
+
+import { useEffect, useState } from "react";
+import { FaTimes } from 'react-icons/fa';
+
+ const Modal = ({
+    modalContent,
+    modalTrigger,
+	  children,
+    className,
+    modalSize = 'w-[100vw] md:w-[100vw] h-[100vh] -ml-[50vw] -mt-[50vh]',
+    triggerModal = () => {},
+}) => {
+
+
+	return (
+        <div className={`fixed w-screen transition-opacity duration-200 ease-in-out h-screen z-40  ${modalTrigger ? 'visible opacity-100' : 'invisible opacity-0'}`}>
+            <div className={`fixed bg-black w-screen h-screen bg-opacity-50 z-30 cursor-pointer backdrop-blur-sm`} onClick={()  => triggerModal(!modalTrigger)}/>
+            <div className={`fixed ${modalSize}  left-1/2 top-1/2 z-50 bg-black bg-opacity-50 ${className}`}>
+				<div className={`fixed cursor-pointer ${modalSize} left-1/2 top-1/2`} onClick={()  => triggerModal(!modalTrigger)}/>
+				{modalContent}
+				{children}
+            </div>
+        </div>
+	);
+};
+
+export default Modal;
